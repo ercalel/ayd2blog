9cbb7c5a55a5cbce8e742c15c7029c96af632ba4
elmer <elmer@debian>
2017-05-19 11:13:14 -0600

    camio de ip a la bd

--- blog/php/conexion.php
+++ blog/php/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "localhost"; //192.168.137.62, 172.17.0.3
+$host = "localhost"; //192.168.137.62, 172.17.0.3 ,     192.168.43.245
 $user = "root";
 $password = "toor";
 $db = "blog";
